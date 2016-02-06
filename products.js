55e10718edcabd185a48581d464e105ca0faf47d
David Cramb <david.cramb@gmail.com>
2016-02-05 21:44:35 -0600

    added six products, console showing objects still undefined

---
+++ products.js
@@ -0,0 +1,43 @@
+var productList = [frozenBanana, popsicles, lemonade, bananagrabber, icecream, cornballs];
+
+var frozenBanana = {
+    name: 'Frozen Banana',
+    description: 'The timeless unbeatable classic!',
+    price: '$10.00',
+    imgurl: 'asdf'
+};
+var popsicles = {
+    name: 'Blu-Popsicle',
+    description: 'Delicious "organic" popsicles! Blueberry only.',
+    price: '$6.00',
+    imgurl: 'asd'
+};
+
+var lemonade = {
+    name: 'Lemonade',
+    description: 'Freshly made from our local lemon grove',
+    price: '$3.50',
+    imgurl: 'ads'
+};
+
+var bananagrabber = {
+    name: 'Banana Grabber Special',
+    description: 'A frozen banana, double dipped in chocolate and there are two of them.',
+    price: '$14.50',
+    imgurl: 'asd'
+};
+
+var icecream = {
+    name: 'Mabey Ice Cream',
+    description: 'Mabey Ice Cream, maybe not.',
+    price: '$6.75 per scoop',
+    imgurl: 'ad'
+};
+
+var cornballs = {
+    name: 'Cornballs',
+    description: 'Fried to perfection, every damn time',
+    price: '$5.00',
+    imgurl: 'ad'
+};
+
