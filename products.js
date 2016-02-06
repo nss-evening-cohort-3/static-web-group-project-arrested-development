970e3610d6203e02bc4e34655e11a26a8267dba1
David Cramb <david.cramb@gmail.com>
2016-02-05 23:04:59 -0600

    restructured code, objects show up in array properly. No img urls yet.

--- products.js
+++ products.js
@@ -1,43 +1,9 @@
-var productList = [frozenBanana, popsicles, lemonade, bananagrabber, icecream, cornballs];
-
-var frozenBanana = {
-    name: 'Frozen Banana',
-    description: 'The timeless unbeatable classic!',
-    price: '$10.00',
-    imgurl: 'asdf'
-};
-var popsicles = {
-    name: 'Blu-Popsicle',
-    description: 'Delicious "organic" popsicles! Blueberry only.',
-    price: '$6.00',
-    imgurl: 'asd'
-};
-
-var lemonade = {
-    name: 'Lemonade',
-    description: 'Freshly made from our local lemon grove',
-    price: '$3.50',
-    imgurl: 'ads'
-};
-
-var bananagrabber = {
-    name: 'Banana Grabber Special',
-    description: 'A frozen banana, double dipped in chocolate and there are two of them.',
-    price: '$14.50',
-    imgurl: 'asd'
-};
-
-var icecream = {
-    name: 'Mabey Ice Cream',
-    description: 'Mabey Ice Cream, maybe not.',
-    price: '$6.75 per scoop',
-    imgurl: 'ad'
-};
-
-var cornballs = {
-    name: 'Cornballs',
-    description: 'Fried to perfection, every damn time',
-    price: '$5.00',
-    imgurl: 'ad'
-};
+var productList = []
+productList[0] = {name: 'Frozen Banana', description: 'The timeless unbeatable classic!', price: 10.00, imgurl: '<a href=*.jpg> </a>'}
+productList[1] = {name: 'Blue-Popsicle', description: 'Delicious and organic popsicles! Blueberry only.', price: 6.00, imgurl: '<a href=*.jpg></a>'}
+productList[2] = {name: 'Lemonade', description: 'Freshly made from our local lemon grove.', price: 3.50, imgurl: '<a href=*.jpg></a>'}
+productList[3] = {name: 'Banana Grabber Special', description: 'A frozen banana, double dipped in chocolate and there are two of them.', price: 14.50, imgurl: '<a href="*.jpg"></a>'}
+productList[4] = {name: 'Mabey Ice Cream', description: 'Mabey Ice Cream, maybe not.', price: 6.75, imgurl: '<a href="*.jpg"></a>'}
+productList[5] = {name: 'Cornballs', description: 'Fried to perfection, watch your hands!', price: 5.00, imgurl: '<a href="*.jpg"></a>'}
+;
 
